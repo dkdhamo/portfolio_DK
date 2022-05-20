b91c2c6a404568eb102fe3284ab5568991dfa351
dkdhamo <82716596+dkdhamo@users.noreply.github.com>
2022-05-20 12:14:36 +0530

    commit

--- js/custom.js
+++ js/custom.js
@@ -103,13 +103,18 @@
 				url: form.attr("action"),
 				method: form.attr("method"),
 				data: form.serialize(),
-				success: function() {
-					 
+				success: function(result) {
+					if (result == "success") {
 						$(".form-inputs").css("display", "none");
 						$(".box p").css("display", "none");
 						$(".contactform").find(".output_message").addClass("success");
 						$(".output_message").text("Message Sent!");
-					
+					} else {
+						$(".tabs-container").css("height", "440px");
+
+						$(".contactform").find(".output_message").addClass("error");
+						$(".output_message").text("Error Sending!");
+					}
 				}
 			});
 
